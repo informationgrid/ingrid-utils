92fb9b7fa7429370d77b94a42336ccc956930c4e
sg <sg@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-09-09 10:38:00 +0000

    add a general IDataSource Interface that need to be implemented by Ibus and Iplug as well.

---
+++ src/java/de/ingrid/utils/IDataSource.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 1997-2005 by media style GmbH
+ * 
+ * $Source: /cvs/asp-search/src/java/com/ms/aspsearch/PermissionDeniedException.java,v $
+ */
+
+package de.ingrid.utils;
+
+public interface IDataSource {
+    public IngridDocument[] search(IngridQuery query, int length) throws Exception;
+}
