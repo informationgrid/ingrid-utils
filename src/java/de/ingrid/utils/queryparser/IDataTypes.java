0e39d1aac56f5db1a84de646cd62fc16bbfd8c7c
sg <sg@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-10-26 14:51:50 +0000

    Interface to hold datatype constants

---
+++ src/java/de/ingrid/utils/queryparser/IDataTypes.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 1997-2005 by media style GmbH
+ * 
+ * $Source: /cvs/asp-search/src/java/com/ms/aspsearch/PermissionDeniedException.java,v $
+ */
+
+package de.ingrid.utils.queryparser;
+
+public interface IDataTypes {
+
+    public final String SNS = "sns";
+}
