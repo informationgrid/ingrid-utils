74353adea2ce8bd2fa98ac5e57f6c8533b6b2f62
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-01-18 11:15:50 +0000

    o fix formatting o add FIXME

--- src/java/de/ingrid/utils/IngridHit.java
+++ src/java/de/ingrid/utils/IngridHit.java
@@ -7,8 +7,8 @@
 package de.ingrid.utils;
 
 /**
- * An container for iplug hits This container does not contain any document
- * details just the information that are required to rank all hits
+ * An container for iplug hits This container does not contain any document details just the information that are
+ * required to rank all hits
  * 
  * created on 09.08.2005
  * 
@@ -17,89 +17,97 @@ package de.ingrid.utils;
  */
 public class IngridHit extends IngridDocument {
 
-	private static final String PROVIDER = "provider";
+    private static final String PROVIDER = "provider";
 
-	private static final String SCORE = "score";
+    private static final String SCORE = "score";
 
-	private static final String DATA_SOURCE_ID = "dataSourceId";
+    private static final String DATA_SOURCE_ID = "dataSourceId";
 
-	public IngridHit() {
-		// default constructor for serialization
-	}
+    /**
+     * 
+     */
+    public IngridHit() {
+        // default constructor for serialization
+    }
 
-	public IngridHit(String provider, int documentId, int dataSourceId,
-			float score) {
-		setProvider(provider);
-		setDocumentId(documentId);
-		setDataSourceId(dataSourceId);
-		setScore(score);
-	}
+    /**
+     * @param provider
+     * @param documentId
+     * @param dataSourceId
+     * @param score
+     */
+    public IngridHit(String provider, int documentId, int dataSourceId, float score) {
+        // FIXME: documentId mismatch here an int in the parent a serializable; this isn't consistent
+        setProvider(provider);
+        setDocumentId(documentId);
+        setDataSourceId(dataSourceId);
+        setScore(score);
+    }
 
-	/**
-	 * sets a id of the internal datasource split
-	 * 
-	 * @param dataSourceId
-	 */
-	public void setDataSourceId(int dataSourceId) {
-		putInt(DATA_SOURCE_ID, dataSourceId);
+    /**
+     * sets a id of the internal datasource split
+     * 
+     * @param dataSourceId
+     */
+    public void setDataSourceId(int dataSourceId) {
+        putInt(DATA_SOURCE_ID, dataSourceId);
 
-	}
+    }
 
-	/**
-	 * @return a data source id, that is used interanaly for one datasource
-	 */
-	public int getDataSourceId() {
-		return getInt(DATA_SOURCE_ID);
-	}
+    /**
+     * @return a data source id, that is used interanaly for one datasource
+     */
+    public int getDataSourceId() {
+        return getInt(DATA_SOURCE_ID);
+    }
 
-	/**
-	 * @return the unique provider Id
-	 */
-	public String getProvider() {
-		return (String) get(PROVIDER);
-	}
+    /**
+     * @return the unique provider Id
+     */
+    public String getProvider() {
+        return (String) get(PROVIDER);
+    }
 
-	/**
-	 * Sets a unique provider Id
-	 * 
-	 * @param providerId
-	 */
-	public void setProvider(String providerId) {
-		put(PROVIDER, providerId);
+    /**
+     * Sets a unique provider Id
+     * 
+     * @param providerId
+     */
+    public void setProvider(String providerId) {
+        put(PROVIDER, providerId);
 
-	}
+    }
 
-	/**
-	 * @return a provider specific document id
-	 */
-	public int getDocumentId() {
-		return getInt(DOCUMENT_ID);
-	}
+    /**
+     * @return a provider specific document id
+     */
+    public int getDocumentId() {
+        return getInt(DOCUMENT_ID);
+    }
 
-	/**
-	 * sets a provider specific document id
-	 * 
-	 * @param documentId
-	 */
-	public void setDocumentId(int documentId) {
-		putInt(DOCUMENT_ID, documentId);
-	}
+    /**
+     * sets a provider specific document id
+     * 
+     * @param documentId
+     */
+    public void setDocumentId(int documentId) {
+        putInt(DOCUMENT_ID, documentId);
+    }
 
-	/**
-	 * Sets the score of a document
-	 * 
-	 * @param score
-	 */
-	public void setScore(float score) {
-		putFloat(SCORE, score);
-	}
+    /**
+     * Sets the score of a document
+     * 
+     * @param score
+     */
+    public void setScore(float score) {
+        putFloat(SCORE, score);
+    }
 
-	/**
-	 * @return the score of this document
-	 * 
-	 */
-	public float getScore() {
-		return getFloat(SCORE);
-
-	}
+    /**
+     * @return the score of this document
+     * 
+     */
+    public float getScore() {
+        return getFloat(SCORE);
+    }
 }
