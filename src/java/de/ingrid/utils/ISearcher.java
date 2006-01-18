32fd42b6905005231d42b680723d3e350dbaeb45
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-01-18 15:30:18 +0000

    o fix typo o add some documentation

--- src/java/de/ingrid/utils/ISearcher.java
+++ src/java/de/ingrid/utils/ISearcher.java
@@ -7,16 +7,17 @@ import de.ingrid.utils.query.IngridQuery;
  * 
  */
 public interface ISearcher {
+
 	/**
 	 * search
 	 * 
 	 * @param query
-	 * @param start
-	 * @param lenght
-	 * @return subset of matching <code>Hit</code>s bundled with meta
-	 *         information into a <code>Hits</code>
+	 * @param start Where to start (range is from 0 to Integer.maxValue).
+	 * @param length
+	 * @return A subset of matching <code>Hit</code>s bundled with meta
+	 *         information into a <code>Hits</code>.
 	 * @throws Exception
 	 */
-	public IngridHits search(IngridQuery query, int start, int lenght)
+	public IngridHits search(IngridQuery query, int start, int length)
 			throws Exception;
 }
