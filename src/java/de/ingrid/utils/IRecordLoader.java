f8515679e73362bf0b70413ff33b96f8c52886ce
sg <sg@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-01-31 10:12:23 +0000

    Add support for remote record loading

---
+++ src/java/de/ingrid/utils/IRecordLoader.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 1997-2005 by media style GmbH
+ * 
+ * $Source: /cvs/asp-search/src/java/com/ms/aspsearch/PermissionDeniedException.java,v $
+ */
+
+package de.ingrid.utils;
+
+import de.ingrid.utils.dbschema.Record;
+
+/**
+ * Interface that can implemented to support complete record loading..
+ * 
+ * created on 09.08.2005
+ * 
+ * @author sg
+ * @version $Revision: 1.3 $
+ */
+public interface IRecordLoader {
+    /**
+     * @param hit
+     * @return
+     * @throws Exception
+     */
+    public Record getRecord(IngridHit hit) throws Exception;
+}
