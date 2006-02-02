7317b72775b94a4f6ea0c09476ddaa468814acda
sg <sg@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-02-02 13:38:46 +0000

    add some standard data types

--- src/java/de/ingrid/utils/queryparser/IDataTypes.java
+++ src/java/de/ingrid/utils/queryparser/IDataTypes.java
@@ -9,4 +9,14 @@ package de.ingrid.utils.queryparser;
 public interface IDataTypes {
 
     public final String SNS = "sns";
+
+    public final String DSC_ECS = "dsc_ecs";
+
+    public final String DSC_ECS_ADDRESS = "dsc_ecs_address";
+
+    public final String DSC_G2K = "dsc_g2k";
+
+    public final String DSC_FS = "dsc_fs";
+
+    public final String DSC_OTHER = "dsc_other";
 }
