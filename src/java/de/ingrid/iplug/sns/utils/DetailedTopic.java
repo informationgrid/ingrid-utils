4e74de0b31956a0ec0dbcc6978091eff71eb8914
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-06-27 10:25:31 +0000

    o add constants for title and definitions of events

--- src/java/de/ingrid/iplug/sns/utils/DetailedTopic.java
+++ src/java/de/ingrid/iplug/sns/utils/DetailedTopic.java
@@ -5,112 +5,132 @@
  */
 package de.ingrid.iplug.sns.utils;
 
-
 /**
  * 
- * created on 21.07.2005 <p>
- *
+ * created on 21.07.2005
+ * <p>
+ * 
  * @author hs
  */
-public class DetailedTopic extends Topic{
-   
+public class DetailedTopic extends Topic {
+
     private static final long serialVersionUID = DetailedTopic.class.getName().hashCode();
+
     /**
      * Constant for administrative identifier.
      */
     public static final String ADMINISTRATIVE_ID = "adminID";
+
     /**
      * Constant for type.
      */
     public static final String TYPE = "type";
+
     /**
-     * Constant for the upper value of the time range. 
+     * Constant for the upper value of the time range.
      */
     public static final String TO = "to";
+
     /**
      * Constant for the lower value of the time range.
      */
     public static final String FROM = "from";
+
+    /**
+     * Constant for definitions.
+     */
+    public static final String DEFINITIONS = "definitions";
     
     public static final String DESCRIPTION_OCC = "descriptionOcc";
 
-    public  static final String SAMPLE_OCC = "sampleOcc";
+    public static final String SAMPLE_OCC = "sampleOcc";
 
-    public static final String ASSICIATED_OCC = "associatedTermsOcc";
+    public static final String ASSOCIATED_OCC = "associatedTermsOcc";
+
     public static final String INSTANCE_OF = "href";
-    
-   
+
+    public static final String TITLES = "titles";
+
     public DetailedTopic(String plugId, int documentId, String topicId, String title, String summary) {
         super(plugId, documentId, topicId, title, summary);
     }
-    
+
     /**
      * 
      */
-    public DetailedTopic(){
+    public DetailedTopic() {
         super();
     }
-    
+
     /**
      * Sets the lower limit of the time range.
+     * 
      * @param from
      */
-    public void setFrom(String from){
+    public void setFrom(String from) {
         put(FROM, from);
     }
-    
+
     /**
      * Allows getter access to the lower limit of the timerange.
-     * @return The value of property from.  
+     * 
+     * @return The value of property from.
      */
-    public String getFrom(){
+    public String getFrom() {
         return (String) get(FROM);
     }
-    
+
     /**
      * Sets the upper limit of the time range.
+     * 
      * @param to
      */
-    public void setTo(String to){
+    public void setTo(String to) {
         put(TO, to);
     }
-    
+
     /**
-     *  Allows getter access to the upper limit of the timerange.
+     * Allows getter access to the upper limit of the timerange.
+     * 
      * @return The value of property to.
      */
-    public String getTo(){
-       return (String) get(TO);
+    public String getTo() {
+        return (String) get(TO);
     }
+
     /**
      * Allows setter access to the type property.
+     * 
      * @param type
      */
-    public void setType(String type){
+    public void setType(String type) {
         put(TYPE, type);
     }
-    
+
     /**
      * Allows getter access to the type property.
+     * 
      * @return The value of property type
      */
-    public String getType(){
+    public String getType() {
         return (String) get(TYPE);
     }
-    
+
     /**
      * Allows setter access to the administrativeID property.
+     * 
      * @param administrativeID
      */
-    public void setAdministrativeID(String administrativeID){
+    public void setAdministrativeID(String administrativeID) {
         put(ADMINISTRATIVE_ID, administrativeID);
     }
-    
+
     /**
-     *  Allows getter access to the administrativeID property.
+     * Allows getter access to the administrativeID property.
+     * 
      * @return The value of property administrativeID.
      */
-    public String getAdministrativeID(){
+    public String getAdministrativeID() {
         return (String) get(ADMINISTRATIVE_ID);
     }
 }
