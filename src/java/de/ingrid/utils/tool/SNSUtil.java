d6218eeb39ace6cbe52efbc761c6d1e251419102
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-10-26 13:16:59 +0000

    - fix KGS transformation drop character[6,7] instead of character[7,8]

--- src/java/de/ingrid/utils/tool/SNSUtil.java
+++ src/java/de/ingrid/utils/tool/SNSUtil.java
@@ -5,39 +5,39 @@ package de.ingrid.utils.tool;
 
 /**
  * TODO Describe your created type (class, etc.) here.
- *
+ * 
  * @author joachim@wemove.com
  */
 public class SNSUtil {
-    
+
     /**
-     * Transforms a SNS location topicId to the internal used ingrid format. 
-     * This format follows the 8-digit kgs-Format (KreisGemeindeSchlüssel) for 
+     * Transforms a SNS location topicId to the internal used ingrid format.
+     * This format follows the 8-digit kgs-Format (KreisGemeindeSchlüssel) for
      * the topic references "GEMEINDE, BUNDESLAND, KREIS".
      * 
-     * Other topic references remain unchanged. 
+     * Other topic references remain unchanged.
      * 
-     * @param topicId The SNS topic ID.
+     * @param topicId
+     *            The SNS topic ID.
      * @return The location reference.
      */
     public static String transformSpacialReference(String topicId) {
-        final String[] TOPIC_REFS = {"GEMEINDE", "BUNDESLAND", "KREIS"};
-        
-        String kgsCode = topicId;  
-        
-        for (int i=0; i< TOPIC_REFS.length; i++) {
+        final String[] TOPIC_REFS = { "GEMEINDE", "BUNDESLAND", "KREIS" };
+
+        String kgsCode = topicId;
+
+        for (int i = 0; i < TOPIC_REFS.length; i++) {
             if (topicId.startsWith(TOPIC_REFS[i])) {
                 kgsCode = topicId.substring(TOPIC_REFS[i].length());
                 if (kgsCode.length() == 10) {
-                    kgsCode = topicId.substring(TOPIC_REFS[i].length(), TOPIC_REFS[i].length() + 6);
-                    kgsCode = kgsCode.concat(topicId.substring(TOPIC_REFS[i].length()+8));
+                    kgsCode = topicId.substring(TOPIC_REFS[i].length(), TOPIC_REFS[i].length() + 5);
+                    kgsCode = kgsCode.concat(topicId.substring(TOPIC_REFS[i].length() + 7));
                 }
                 break;
             }
         }
-        
+
         return kgsCode;
     }
-    
 
 }
--- src/test/de/ingrid/utils/tool/SNSUtilTest.java
+++ src/test/de/ingrid/utils/tool/SNSUtilTest.java
@@ -8,7 +8,7 @@ public class SNSUtilTest extends TestCase {
      * Test method for 'de.ingrid.utils.tool.SNSUtil.transformSpacialReference(String)'
      */
     public void testTransformSpacialReference() {
-        assertEquals("12345690", SNSUtil.transformSpacialReference("GEMEINDE1234567890"));
+        assertEquals("12345890", SNSUtil.transformSpacialReference("GEMEINDE1234567890"));
         assertEquals("12", SNSUtil.transformSpacialReference("BUNDESLAND12"));
         assertEquals("12345", SNSUtil.transformSpacialReference("KREIS12345"));
         assertEquals("SCHUTZGEBIET170", SNSUtil.transformSpacialReference("SCHUTZGEBIET170"));
