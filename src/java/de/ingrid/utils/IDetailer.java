43d97f5bfc241f82cc2f8a94bf0e21b40df92a5a
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-12-01 09:18:37 +0000

    o some fixes and styles for javadoc

--- src/java/de/ingrid/utils/IDetailer.java
+++ src/java/de/ingrid/utils/IDetailer.java
@@ -3,33 +3,41 @@ package de.ingrid.utils;
 import de.ingrid.utils.query.IngridQuery;
 
 /**
- * General Detailer interface to get details of a IngridHit document
- * 
+ * General Detailer interface to get details of a IngridHit document.
  */
-public interface IDetailer
- {
+public interface IDetailer {
 
-	/**
-	 * @param hit the hit document
-     * @param query a ingridquery to caculate the summary
-     * @param requestedFields fields the detailer should puhs into the detail object, if this fields are unknow just igrnore it
-	 * @return a detailed document of a hit
-	 * @throws Exception
-	 */
-	public IngridHitDetail  getDetail(IngridHit hit, IngridQuery query, String[] requestedFields) throws Exception;
-
-    
-    
     /**
-     * @param hits
+     * Searches for details to a hit.
+     * 
+     * @param hit
+     *            The from search returned hit document.
      * @param query
-     * @param requestedFields fields the detailer should puhs into the detail object, if this fields are unknow just igrnore it
-     * @return an array of details or a emthy array.
+     *            The query for the hit to caculate a summary.
+     * @param requestedFields
+     *            Fields the detailer should push into the detail object. This can be set to NULL if it isn't needed.
+     * @return A constructed document with details to a hit.
      * @throws Exception
      */
-    public IngridHitDetail[]  getDetails(IngridHit[] hits, IngridQuery query, String[] requestedFields) throws Exception;
+    public IngridHitDetail getDetail(IngridHit hit, IngridQuery query, String[] requestedFields) throws Exception;
 
+    /**
+     * Searches for details to a array of hits.
+     * 
+     * @param hits
+     *            The array of hit documents from a search.
+     * @param query
+     *            The query for the hit to caculate a summary.
+     * @param requestedFields
+     *            Fields the detailer should push into the detail object. This can be set to NULL if it isn't needed.
+     * @return An array of constructed documents with details to a hit. If nothing is found the array is empty.
+     * @throws Exception
+     */
+    public IngridHitDetail[] getDetails(IngridHit[] hits, IngridQuery query, String[] requestedFields) throws Exception;
 
-
-    public void close() throws Exception;    
+    /**
+     * Closes the detailer.
+     * @throws Exception If something goes wrong during close.
+     */
+    public void close() throws Exception;
 }
