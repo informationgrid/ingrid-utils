942c28a43a7dbeee1aa8eee4b31fdc55c9a39607
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2007-03-06 11:46:39 +0000

    - add more funtionality for   * reverse domain lookup (get ID from value)   * transformation of codelist 505 values to UDK values - add testcases

--- src/java/de/ingrid/utils/udk/UtilsUDKCodeLists.java
+++ src/java/de/ingrid/utils/udk/UtilsUDKCodeLists.java
@@ -5,6 +5,8 @@ package de.ingrid.utils.udk;
 
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import de.ingrid.utils.xml.XMLSerializer;
 
@@ -16,6 +18,8 @@ import de.ingrid.utils.xml.XMLSerializer;
 public class UtilsUDKCodeLists {
 
     private static final HashMap codeLists;
+    
+    private static final HashMap udk2codeList505Map;
 
     static {
         try {
@@ -29,8 +33,40 @@ public class UtilsUDKCodeLists {
         } catch (Throwable ex) {
             throw new ExceptionInInitializerError(ex);
         }
+        
+        /* mapping of UDK addresstypes to CSW address types
+        
+        UDK | CSW (codelist 505)| Name
+        0 | 7 | Auskunft
+        1 | 3 | Datenhalter
+        2 | 2 | Datenverantwortung
+        3 | 1 | Anbieter
+        4 | 4 | Benutzer
+        5 | 5 | Vertrieb
+        6 | 6 | Herkunft
+        7 | 8 | Datenerfassung
+        8 | 9 | Auswertung
+        9 | 10 | Herausgeber
+        999 | keine Entsprechung, mapping auf codeListValue | Sonstige Angaben
+         */
+        udk2codeList505Map = new HashMap();
+        udk2codeList505Map.put("0", "7");
+        udk2codeList505Map.put("1", "3");
+        udk2codeList505Map.put("3", "1");
+        udk2codeList505Map.put("7", "8");
+        udk2codeList505Map.put("8", "9");
+        udk2codeList505Map.put("9", "10");
     }
     
+    /**
+     * Return the code list value specified by the codelist id, the domain id and the language id.
+     * If the value cannot be found, return "".
+     * 
+     * @param codeListId
+     * @param domainId
+     * @param langId
+     * @return
+     */
     public static String getCodeListEntryName(Long codeListId, Long domainId, Long langId) {
         try {
             return (String)((HashMap)((HashMap)codeLists.get(codeListId)).get(domainId)).get(langId);
@@ -38,7 +74,56 @@ public class UtilsUDKCodeLists {
             return "";
         }
     }
+    
+    /**
+     * Return the domain id specified by the codeListId, domainValue, language Id.
+     * If the domain id cannot be found, return null.
+     * 
+     * @param codeListId
+     * @param domainValue
+     * @param langId
+     * @return
+     */
+    public static String getCodeListDomainId(Long codeListId, String domainValue, Long langId) {
+        HashMap domain = (HashMap)codeLists.get(codeListId);
+        for (Iterator it = domain.entrySet().iterator(); it.hasNext();) {
+            Map.Entry domainEntry = (Map.Entry)it.next();
+            String domainEntryValue = (String)((HashMap)domainEntry.getValue()).get(langId); 
+            if (domainEntryValue.equalsIgnoreCase(domainValue)) {
+                return domainEntry.getKey().toString();
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Return mapped value from the codeList505 to UDK mapping.
+     * 
+     * @param code
+     * @return
+     */
+    public static String codeList505ToUDK(String code) {
+        if (udk2codeList505Map.containsValue(code)) {
+            for (Iterator it =udk2codeList505Map.entrySet().iterator(); it.hasNext();) {
+                Map.Entry entry = (Map.Entry)it.next();
+                if (((String)entry.getValue()).equals(code)) {
+                    return (String)entry.getKey();
+                }
+            }
+        }
+        return code;
+    }
 
-    
-    
+    /**
+     * Return mapped value from the UDK to codeList505 mapping.
+     * 
+     * @param code
+     * @return
+     */
+    public static String udkToCodeList505(String code) {
+        if (udk2codeList505Map.containsKey(code)) {
+            return (String)udk2codeList505Map.get(code);
+        }
+        return code;
+    }
 }
--- src/test/de/ingrid/utils/udk/UtilsUDKCodeListsTest.java
+++ src/test/de/ingrid/utils/udk/UtilsUDKCodeListsTest.java
@@ -16,4 +16,68 @@ public class UtilsUDKCodeListsTest extends TestCase {
         assertEquals(true, languageName.equals("Englisch"));
     }
 
+    public void testGetCodeListDomainId() {
+        
+        String languageName = UtilsUDKCodeLists.getCodeListDomainId(new Long(99999999), "englisch", new Long(121));
+        assertEquals(true, languageName.equals("94"));
+    }
+    
+    
+    public void testCodeList505ToUDK() {
+        /* mapping of UDK addresstypes to CSW address types
+        
+        UDK | CSW (codelist 505)| Name
+        0 | 7 | Auskunft
+        1 | 3 | Datenhalter
+        2 | 2 | Datenverantwortung
+        3 | 1 | Anbieter
+        4 | 4 | Benutzer
+        5 | 5 | Vertrieb
+        6 | 6 | Herkunft
+        7 | 8 | Datenerfassung
+        8 | 9 | Auswertung
+        9 | 10 | Herausgeber
+        999 | keine Entsprechung, mapping auf codeListValue | Sonstige Angaben
+         */
+        assertEquals(UtilsUDKCodeLists.codeList505ToUDK("7"), "0");
+        assertEquals(UtilsUDKCodeLists.codeList505ToUDK("3"), "1");
+        assertEquals(UtilsUDKCodeLists.codeList505ToUDK("2"), "2");
+        assertEquals(UtilsUDKCodeLists.codeList505ToUDK("1"), "3");
+        assertEquals(UtilsUDKCodeLists.codeList505ToUDK("4"), "4");
+        assertEquals(UtilsUDKCodeLists.codeList505ToUDK("5"), "5");
+        assertEquals(UtilsUDKCodeLists.codeList505ToUDK("6"), "6");
+        assertEquals(UtilsUDKCodeLists.codeList505ToUDK("8"), "7");
+        assertEquals(UtilsUDKCodeLists.codeList505ToUDK("9"), "8");
+        assertEquals(UtilsUDKCodeLists.codeList505ToUDK("10"), "9");
+    }
+
+    public void testUdkToCodeList505() {
+        /* mapping of UDK addresstypes to CSW address types
+        
+        UDK | CSW (codelist 505)| Name
+        0 | 7 | Auskunft
+        1 | 3 | Datenhalter
+        2 | 2 | Datenverantwortung
+        3 | 1 | Anbieter
+        4 | 4 | Benutzer
+        5 | 5 | Vertrieb
+        6 | 6 | Herkunft
+        7 | 8 | Datenerfassung
+        8 | 9 | Auswertung
+        9 | 10 | Herausgeber
+        999 | keine Entsprechung, mapping auf codeListValue | Sonstige Angaben
+         */
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("0"), "7");
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("1"), "3");
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("2"), "2");
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("3"), "1");
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("4"), "4");
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("5"), "5");
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("6"), "6");
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("7"), "8");
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("8"), "9");
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("9"), "10");
+        assertEquals(UtilsUDKCodeLists.udkToCodeList505("999"), "999");
+    }
+
 }
