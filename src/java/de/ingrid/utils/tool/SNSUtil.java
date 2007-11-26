92e9980fb1dad2425086d84873992609d72bafa2
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2007-11-26 11:15:26 +0000

    o extract now the kgs from the nativeKey

--- src/java/de/ingrid/utils/tool/SNSUtil.java
+++ src/java/de/ingrid/utils/tool/SNSUtil.java
@@ -11,33 +11,26 @@ package de.ingrid.utils.tool;
 public class SNSUtil {
 
     /**
-     * Transforms a SNS location topicId to the internal used ingrid format.
-     * This format follows the 8-digit kgs-Format (KreisGemeindeSchlüssel) for
-     * the topic references "GEMEINDE, BUNDESLAND, KREIS".
+     * Transforms a SNS location topicId to the internal used ingrid format. This format follows the 8-digit kgs-Format
+     * (KreisGemeindeSchlüssel) for the topic references "GEMEINDE, BUNDESLAND, KREIS".
      * 
      * Other topic references remain unchanged.
      * 
-     * @param topicId
-     *            The SNS topic ID.
+     * @param nativeKey
+     *            The SNS native key.
      * @return The location reference.
      */
-    public static String transformSpacialReference(String topicId) {
-        final String[] TOPIC_REFS = { "GEMEINDE", "BUNDESLAND", "KREIS" };
+    public static String transformSpacialReference(String nativeKey) {
+        String agsCode = nativeKey;
 
-        String kgsCode = topicId;
-
-        for (int i = 0; i < TOPIC_REFS.length; i++) {
-            if (topicId.startsWith(TOPIC_REFS[i])) {
-                kgsCode = topicId.substring(TOPIC_REFS[i].length());
-                if (kgsCode.length() == 10) {
-                    kgsCode = topicId.substring(TOPIC_REFS[i].length(), TOPIC_REFS[i].length() + 5);
-                    kgsCode = kgsCode.concat(topicId.substring(TOPIC_REFS[i].length() + 7));
-                }
-                break;
-            }
+        final String agsString = "ags:";
+        int index = nativeKey.indexOf(agsString);
+        if (index != -1) {
+            final int startIndex = index + agsString.length();
+            agsCode = nativeKey.substring(startIndex, startIndex + 7);
         }
 
-        return kgsCode;
+        return agsCode;
     }
 
 }
