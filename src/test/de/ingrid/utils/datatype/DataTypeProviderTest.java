13bd2f6432b34f92655f65bd30251b163e1485c7
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-01-29 10:51:24 +0000

    fix test

--- src/test/de/ingrid/utils/datatype/DataTypeProviderTest.java
+++ src/test/de/ingrid/utils/datatype/DataTypeProviderTest.java
@@ -1,17 +1,20 @@
 package de.ingrid.utils.datatype;
 
+import java.io.File;
+
 import junit.framework.TestCase;
 
-public class DataTypeProviderTest extends TestCase{
+public class DataTypeProviderTest extends TestCase {
 
     public void testLoadDatatypes() throws Exception {
-       IDataTypeProvider datatypeProvider = new DataTypeProvider(new DataTypeEditor());
-       DataType[] dataTypes = datatypeProvider.getDataTypes();
-       assertNotNull(dataTypes);
-       assertEquals(1, dataTypes.length);
-       DataType dataType = dataTypes[0];
-       assertEquals("foo", dataType.getName());
-       assertEquals("bar", dataType.getDisplayName());
-       assertTrue(dataType.isVisible());
+        IDataTypeProvider datatypeProvider = new DataTypeProvider(new File("src/test-resources/datatypes.properties"),
+                new DataTypeEditor());
+        DataType[] dataTypes = datatypeProvider.getDataTypes();
+        assertNotNull(dataTypes);
+        assertEquals(1, dataTypes.length);
+        DataType dataType = dataTypes[0];
+        assertEquals("foo", dataType.getName());
+        assertEquals("bar", dataType.getDisplayName());
+        assertTrue(dataType.isVisible());
     }
 }
