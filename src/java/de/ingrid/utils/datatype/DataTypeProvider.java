6d9ea8bbf8886c74b3344714d788b49ea40a17d4
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-01-29 10:48:31 +0000

    inject datatype file to extract Datatypes

--- src/java/de/ingrid/utils/datatype/DataTypeProvider.java
+++ src/java/de/ingrid/utils/datatype/DataTypeProvider.java
@@ -2,9 +2,9 @@ package de.ingrid.utils.datatype;
 
 import java.beans.PropertyEditor;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,21 +13,23 @@ import org.apache.commons.logging.LogFactory;
 
 public class DataTypeProvider implements IDataTypeProvider {
 
-    private static final String DATA_TYPE_FILE_NAME = "datatypes.properties";
-
     private static final Log LOG = LogFactory.getLog(DataTypeProvider.class);
 
     private final PropertyEditor _propertyEditor;
 
-    public DataTypeProvider(PropertyEditor propertyEditor) {
+    private final File _datatypeProperty;
+
+    public DataTypeProvider(File datatypeProperty, PropertyEditor propertyEditor) {
+        _datatypeProperty = datatypeProperty;
         _propertyEditor = propertyEditor;
     }
 
     public DataType[] getDataTypes() {
-        InputStream resourceAsStream = DataTypeProvider.class.getResourceAsStream("/" + DATA_TYPE_FILE_NAME);
         List datatypes = new ArrayList();
+        FileReader fileReader = null;
         try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceAsStream));
+            fileReader = new FileReader(_datatypeProperty);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = null;
             while ((line = bufferedReader.readLine()) != null) {
                 _propertyEditor.setAsText(line);
@@ -39,8 +41,8 @@ public class DataTypeProvider implements IDataTypeProvider {
             LOG.warn("can not load datatype file.", e);
         } finally {
             try {
-                if (resourceAsStream != null) {
-                    resourceAsStream.close();
+                if (fileReader != null) {
+                    fileReader.close();
                 }
             } catch (IOException e) {
                 LOG.warn("can not close datatype-resource-stream.", e);
