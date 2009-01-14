2ceef53a5376ea7bfa7b4c54d6932fb0cfe24505
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-01-14 12:09:19 +0000

    factory has new constructor to get and provide ibus to all injectors

---
+++ src/java/de/ingrid/utils/metadata/IBusable.java
@@ -0,0 +1,8 @@
+package de.ingrid.utils.metadata;
+
+import de.ingrid.utils.IBus;
+
+public interface IBusable {
+
+	void setIBus(IBus bus);
+}
--- src/java/de/ingrid/utils/metadata/MetadataInjectorFactory.java
+++ src/java/de/ingrid/utils/metadata/MetadataInjectorFactory.java
@@ -3,14 +3,18 @@ package de.ingrid.utils.metadata;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.ingrid.utils.IBus;
 import de.ingrid.utils.PlugDescription;
 
 public class MetadataInjectorFactory {
 
 	private final PlugDescription _description;
+	private final IBus _bus;
 
-	public MetadataInjectorFactory(PlugDescription description) {
+	public MetadataInjectorFactory(final PlugDescription description,
+			final IBus bus) {
 		_description = description;
+		_bus = bus;
 	}
 
 	public List<IMetadataInjector> getMetadataInjectors() throws Exception {
@@ -23,6 +27,10 @@ public class MetadataInjectorFactory {
 						.forName(injectorName);
 				IMetadataInjector metadataInjector = clazz.newInstance();
 				metadataInjector.configure(_description);
+				if (metadataInjector instanceof IBusable) {
+					((IBusable) metadataInjector).setIBus(_bus);
+				}
+				
 				list.add(metadataInjector);
 			}
 		}
