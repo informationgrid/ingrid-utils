2bdf6a0221f50f5f80780489cce4740998cad0b5
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-01-15 13:32:48 +0000

    add configure method

--- src/java/de/ingrid/utils/metadata/IPlugOperatorFinder.java
+++ src/java/de/ingrid/utils/metadata/IPlugOperatorFinder.java
@@ -3,10 +3,14 @@ package de.ingrid.utils.metadata;
 import java.io.IOException;
 import java.util.Set;
 
+import de.ingrid.utils.PlugDescription;
+
 public interface IPlugOperatorFinder {
 
     Set<String> findPartner() throws IOException;
     
     Set<String> findProvider() throws IOException;
+    
+    void configure(PlugDescription plugDescription);
 
 }
