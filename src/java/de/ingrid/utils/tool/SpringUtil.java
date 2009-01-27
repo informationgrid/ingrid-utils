aad66f37ff9a13ea8543a922a1472767e156d462
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-01-27 10:21:30 +0000

    fix file not found

--- src/java/de/ingrid/utils/tool/SpringUtil.java
+++ src/java/de/ingrid/utils/tool/SpringUtil.java
@@ -6,17 +6,23 @@ import org.springframework.core.io.Resource;
 
 public class SpringUtil {
 
-	private XmlBeanFactory _xmlBeanFactory;
+    private XmlBeanFactory _xmlBeanFactory;
 
-	public SpringUtil(String fileName) {
-		Resource resource = new ClassPathResource(fileName);
-		_xmlBeanFactory = new XmlBeanFactory(resource);
-	}
+    public SpringUtil(String fileName) {
+        Resource resource = new ClassPathResource(fileName);
+        if (resource.exists()) {
+            _xmlBeanFactory = new XmlBeanFactory(resource);
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	public <T> T getBean(String beanName, Class<T> clazz) {
-		Object bean = _xmlBeanFactory.getBean(beanName);
-		return (T) bean;
-	}
+    @SuppressWarnings("unchecked")
+    public <T> T getBean(String beanName, Class<T> clazz) {
+        T t = null;
+        if (_xmlBeanFactory != null) {
+            Object bean = _xmlBeanFactory.getBean(beanName);
+            t = (T) bean;
+        }
+        return t;
+    }
 
 }
