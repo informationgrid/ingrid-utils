2b9924f4bd12ff2b016ee3db14653c4d679e8606
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-01-27 11:10:24 +0000

    add debugging

--- src/java/de/ingrid/utils/processor/impl/QueryExtensionPreProcessor.java
+++ src/java/de/ingrid/utils/processor/impl/QueryExtensionPreProcessor.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.impl.LogFactoryImpl;
+
 import de.ingrid.utils.IConfigurable;
 import de.ingrid.utils.PlugDescription;
 import de.ingrid.utils.processor.IPreProcessor;
@@ -16,14 +19,22 @@ public class QueryExtensionPreProcessor implements IPreProcessor, IConfigurable 
 
     private Map<String, List<FieldQuery>> _queryExtension = new HashMap<String, List<FieldQuery>>();
 
+    private static final Log LOG = LogFactoryImpl.getLog(QueryExtensionPreProcessor.class);
+
     @Override
     public void process(IngridQuery query) throws Exception {
         String busUrl = (String) query.get(BUS_URL);
         if (_queryExtension.containsKey(busUrl)) {
             List<FieldQuery> fieldQueries = _queryExtension.get(busUrl);
             for (FieldQuery fieldQuery : fieldQueries) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("add field to query: " + fieldQuery);
+                }
                 query.addField(fieldQuery);
             }
+        }
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("pre process finished: " + query);
         }
     }
 
