c6149b96ba43c385b8d756e1936dab26502fd67b
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-01-27 14:44:28 +0000

    INGRID-1616 implement queryextension object

---
+++ src/java/de/ingrid/utils/QueryExtension.java
@@ -0,0 +1,64 @@
+package de.ingrid.utils;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.HashSet;
+import java.util.Set;
+
+import de.ingrid.utils.query.FieldQuery;
+
+public class QueryExtension implements Externalizable {
+
+    private String _busUrl;
+
+    private Set<FieldQuery> _fieldQueries = new HashSet<FieldQuery>();
+
+    public QueryExtension() {
+        // nothing todo
+    }
+
+    public String getBusUrl() {
+        return _busUrl;
+    }
+
+    public void setBusUrl(String busUrl) {
+        _busUrl = busUrl;
+    }
+
+    public Set<FieldQuery> getFieldQueries() {
+        return _fieldQueries;
+    }
+
+    public void addFieldQuery(FieldQuery fieldQuery) {
+        _fieldQueries.add(fieldQuery);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        _fieldQueries.clear();
+        _busUrl = (String) in.readObject();
+        int count = in.readInt();
+        for (int i = 0; i < count; i++) {
+            boolean required = in.readBoolean();
+            boolean prohibited = in.readBoolean();
+            String fieldName = (String) in.readObject();
+            String fieldValue = (String) in.readObject();
+            _fieldQueries.add(new FieldQuery(required, prohibited, fieldName, fieldValue));
+        }
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(_busUrl);
+        out.writeInt(_fieldQueries.size());
+        for (FieldQuery fieldQuery : _fieldQueries) {
+            out.writeBoolean(fieldQuery.isRequred());
+            out.writeBoolean(fieldQuery.isProhibited());
+            out.writeObject(fieldQuery.getFieldName());
+            out.writeObject(fieldQuery.getFieldValue());
+        }
+    }
+
+}
