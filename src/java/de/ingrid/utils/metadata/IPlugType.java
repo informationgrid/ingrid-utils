e356cad529cdc63827b91140663ada6db1b05d8a
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-02-02 15:44:57 +0000

    new iplug types

--- src/java/de/ingrid/utils/metadata/IPlugType.java
+++ src/java/de/ingrid/utils/metadata/IPlugType.java
@@ -2,5 +2,5 @@ package de.ingrid.utils.metadata;
 
 public enum IPlugType {
 
-	IPLUG_SE, IPLUG_DSC, IPLUG_SNS, IPLUG_TAMINO, OTHER
+    IPLUG_SE, IPLUG_DSC, IPLUG_SNS, IPLUG_TAMINO, IPLUG_FPN, IPLUG_G2K, OTHER
 }
