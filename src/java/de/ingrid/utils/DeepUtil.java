61141c2309aab7a8abeb1c94f566b3d61bb14601
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-02-05 08:44:03 +0000

    fix deepString

--- src/java/de/ingrid/utils/DeepUtil.java
+++ src/java/de/ingrid/utils/DeepUtil.java
@@ -9,10 +9,10 @@ public class DeepUtil {
         if ((object instanceof Object[]) && deep < MAX_DEEP) {
             Object[] objects = (Object[]) object;
             for (Object object2 : objects) {
-                ret = "_" + deepString(object2, deep + 1);
+                ret += "_" + deepString(object2, deep + 1);
             }
         } else {
-            ret = "_" + ((object == null) ? "" : object.toString());
+            ret += "_" + ((object == null) ? "" : object.toString());
         }
         return ret;
     }
