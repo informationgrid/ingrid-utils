a9a6316012e575dd1cfbf50159a6361105f074ed
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-02-06 08:49:56 +0000

    implement deepHashcode

--- src/java/de/ingrid/utils/DeepUtil.java
+++ src/java/de/ingrid/utils/DeepUtil.java
@@ -1,8 +1,13 @@
 package de.ingrid.utils;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.impl.LogFactoryImpl;
+
 public class DeepUtil {
 
     public static final int MAX_DEEP = 50;
+
+    private static final Log LOG = LogFactoryImpl.getLog(DeepUtil.class);
 
     public static String deepString(Object object, int deep) {
         String ret = "";
@@ -18,4 +23,22 @@ public class DeepUtil {
         }
         return ret;
     }
+    
+    public static int deepHashcode(Object object, int deep) {
+        final int prime = 31;
+        int result = 1;
+        if ((object instanceof Object[]) && deep < MAX_DEEP) {
+            Object[] objects = (Object[]) object;
+            for (Object object2 : objects) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("compute hashcode recursive with deep: " + deep);
+                }
+                result = prime * result + deepHashcode(object2, deep + 1);
+            }
+        } else {
+            result = prime * result + ((object == null) ? 0 : object.hashCode());
+        }
+        return result;
+    }
+    
 }
