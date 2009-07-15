38bf334b40a166832c9c8f1012a1d05f0a82baae
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-07-15 14:02:18 +0000

    - improve date detection

--- src/main/java/de/ingrid/utils/udk/UtilsCSWDate.java
+++ src/main/java/de/ingrid/utils/udk/UtilsCSWDate.java
@@ -49,6 +49,8 @@ public class UtilsCSWDate {
 			datePattern = "yyyy-MM-dd";
 		} else if (dateString.matches("[0-9][0-9][0-9][0-9][0-1][0-9][0-3][0-9]T[0-2][0-9][0-5][0-9][0-5][0-9]")) {
 			datePattern = "yyyyMMdd'T'HHmmss";
+		} else if (dateString.matches("[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9]")) {
+			datePattern = "yyyy-MM-dd'T'HH:mm:ss";
 		}
 		return datePattern;
 	}
