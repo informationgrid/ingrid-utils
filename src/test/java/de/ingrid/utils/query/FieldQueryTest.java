1a506f1f0b3d1893305d060aba0408961c9c0847
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-10-08 06:59:25 +0000

    + implement test  to verify collision + fix collision

---
+++ src/test/java/de/ingrid/utils/query/FieldQueryTest.java
@@ -0,0 +1,12 @@
+package de.ingrid.utils.query;
+
+import junit.framework.TestCase;
+
+public class FieldQueryTest extends TestCase {
+
+	public void testCollisoion() throws Exception {
+		FieldQuery fieldQuery1 = new FieldQuery(true, false, "partner", "bw");
+		FieldQuery fieldQuery2 = new FieldQuery(true, false, "partner", "be");
+		assertFalse(fieldQuery1.hashCode() == fieldQuery2.hashCode());
+	}
+}
