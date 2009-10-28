87fae2bfe5d2b111303876d587fdf8c3e656194f
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-10-28 08:03:06 +0000

    avoid npe

--- src/main/java/de/ingrid/utils/metadata/DefaultIPlugOperatorInjector.java
+++ src/main/java/de/ingrid/utils/metadata/DefaultIPlugOperatorInjector.java
@@ -9,37 +9,37 @@ import de.ingrid.utils.PlugDescription;
 
 public class DefaultIPlugOperatorInjector extends AbstractIPlugOperatorInjector {
 
-	private PlugDescription _plugDescription;
+    private PlugDescription _plugDescription;
 
-	@Override
-	public IPlugOperatorFinder createOperatorFinder() {
-		
-		IPlugOperatorFinder finder = new IPlugOperatorFinder() {
+    @Override
+    public IPlugOperatorFinder createOperatorFinder() {
 
-			@Override
-			public Set<String> findProvider() throws IOException {
-				String[] providers = _plugDescription.getProviders();
-				Set<String> set = new HashSet<String>(Arrays.asList(providers));
-				return set;
-			}
+        IPlugOperatorFinder finder = new IPlugOperatorFinder() {
 
-			@Override
-			public Set<String> findPartner() throws IOException {
-				String[] partners = _plugDescription.getPartners();
-				Set<String> set = new HashSet<String>(Arrays.asList(partners));
-				return set;
-			}
+            @Override
+            public Set<String> findProvider() throws IOException {
+                String[] providers = _plugDescription != null ? _plugDescription.getProviders() : new String[] {};
+                Set<String> set = new HashSet<String>(Arrays.asList(providers));
+                return set;
+            }
 
-			@Override
-			public void configure(PlugDescription plugDescription) {
-			}
-		};
-		return finder;
-	}
+            @Override
+            public Set<String> findPartner() throws IOException {
+                String[] partners = _plugDescription != null ? _plugDescription.getPartners() : new String[] {};
+                Set<String> set = new HashSet<String>(Arrays.asList(partners));
+                return set;
+            }
 
-	@Override
-	public void configure(PlugDescription description) {
-		_plugDescription = description;
-	}
+            @Override
+            public void configure(PlugDescription plugDescription) {
+            }
+        };
+        return finder;
+    }
+
+    @Override
+    public void configure(PlugDescription description) {
+        _plugDescription = description;
+    }
 
 }
