0cde6991f6e81d256bc34cc269a569ca685be67b
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2011-03-28 12:41:14 +0000

    utils: - modified StringUtil functionality for replacing line feeds, now with trim()

--- src/main/java/de/ingrid/utils/tool/StringUtil.java
+++ src/main/java/de/ingrid/utils/tool/StringUtil.java
@@ -8,13 +8,14 @@ package de.ingrid.utils.tool;
  */
 public class StringUtil {
 
-    /** Replace all "\n" and "\r" with the given string
+    /** Make a string to a "one liner" !
+     * Replaces all "\n" and "\r" with the given string. ALSO removes leading whitespaces (trim()) !
      * @param inString String where line feeds will be replaced
      * @param replaceWith The string to replace line feeds with
      * @return
      */
     public static String replaceLineFeeds(String inString, String replaceWith) {
-        return inString.replace("\n", replaceWith).replace("\r", replaceWith);
+        return inString.replace("\n", replaceWith).replace("\r", replaceWith).trim();
     }
 
 }
--- src/test/java/de/ingrid/utils/tool/StringUtilTest.java
+++ src/test/java/de/ingrid/utils/tool/StringUtilTest.java
@@ -8,6 +8,6 @@ public class StringUtilTest extends TestCase {
     	String inString = "line1\nline2\rline3\n\r";
 //    	System.out.println("testReplaceLineFeeds with String '" + inString + "'");
         assertEquals("line1line2line3", StringUtil.replaceLineFeeds(inString, ""));
-        assertEquals("line1 line2 line3", StringUtil.replaceLineFeeds(inString, " ").trim());
+        assertEquals("line1 line2 line3", StringUtil.replaceLineFeeds(inString, " "));
     }
 }
