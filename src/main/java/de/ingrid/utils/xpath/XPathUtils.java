f0974410b21728fe0caf7243d7bde152b227a38f
ih <ih@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2011-09-19 16:17:29 +0000

    added new XPathUtils class (removed singleton pattern)

--- src/main/java/de/ingrid/utils/xpath/XPathUtils.java
+++ src/main/java/de/ingrid/utils/xpath/XPathUtils.java
@@ -1,4 +1,4 @@
-package de.ingrid.utils.xml;
+package de.ingrid.utils.xpath;
 
 import javax.xml.namespace.NamespaceContext;
 import javax.xml.xpath.XPath;
@@ -11,110 +11,59 @@ import org.apache.commons.logging.LogFactory;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-/**
- * @deprecated Use de.ingrid.utils.xpath.XPathUtils instead
- */
-@Deprecated
+import de.ingrid.utils.xml.XMLUtils;
+
 public class XPathUtils {
 
 	final protected static Log log = LogFactory.getLog(XPathUtils.class);
 
-	private static NamespaceContext nsContext = new Csw202NamespaceContext();
+	private XPath xpath = null;
 
-	private static XPath xpath = null;
-
-	private static XPathUtils myInstance;
-
-	private XPathUtils() {}
-
-	/** Get The Singleton. */
-	public static synchronized XPathUtils getInstance() {
-		if (myInstance == null) {
-			myInstance = new XPathUtils();
-		}
-		return myInstance;
+	/**
+	 * Constructor
+	 * @param nsContext NamespaceContext to use
+	 */
+	public XPathUtils(NamespaceContext nsContext) {
+		this.xpath = XPathFactory.newInstance().newXPath();
+		this.xpath.setNamespaceContext(nsContext);
 	}
 
-	/** Get The Singleton. Initialize with given NamespaceContext (Default). */
-	public static synchronized XPathUtils getInstance(NamespaceContext nsContext) {
-		if (myInstance == null) {
-			myInstance = new XPathUtils();
-		}
-		myInstance.nsContext = nsContext;
-
-		return myInstance;
-	}
-
-	/** Get XPath instance with default namespaces. */
-	public static XPath getXPathInstance() {
-		if (xpath == null) {
-			xpath = createNewXPathInstance();
-			xpath.setNamespaceContext(nsContext);
-		}
-
-		return xpath;
-	}
-
-	/** Get XPath instance with given namespaces. Pass null if default namespaces. */
-	public static XPath getXPathInstance(NamespaceContext nsContext) {
-		if (nsContext != null){
-			if (xpath == null) {
-				xpath = createNewXPathInstance();
-			}
-			// always set context ! might be a new one !
-			xpath.setNamespaceContext(nsContext);
-		}else{
-			getXPathInstance();
-		}
-
-		return xpath;
-	}
-
-	private static XPath createNewXPathInstance() {
-		return XPathFactory.newInstance().newXPath();
-	}
-
-	public static Integer getInt(Object source, String xpathExpression) {
-		String value = getString(source, xpathExpression);
+	public Integer getInt(Object source, String xpathExpression) {
+		String value = this.getString(source, xpathExpression);
 
 		if (value != null) {
 			return Integer.valueOf(value);
-
 		} else {
 			return null;
 		}
 	}
 
-	public static Double getDouble(Object source, String xpathExpression) {
-		String value = getString(source, xpathExpression);
+	public Double getDouble(Object source, String xpathExpression) {
+		String value = this.getString(source, xpathExpression);
 
 		if (value != null) {
 			return Double.valueOf(value);
-
 		} else {
 			return null;
 		}
 	}
 
-	public static Long getLong(Object source, String xpathExpression) {
-		String value = getString(source, xpathExpression);
+	public Long getLong(Object source, String xpathExpression) {
+		String value = this.getString(source, xpathExpression);
 
 		if (value != null) {
 			return Long.valueOf(value);
-
 		} else {
 			return null;
 		}
 	}
 
-	public static boolean nodeExists(Object source, String xpathExpression) {
+	public boolean nodeExists(Object source, String xpathExpression) {
 		try {
 			if (source != null) {
-				XPath xpath = getXPathInstance();
-				Boolean exists = (Boolean) xpath.evaluate(xpathExpression, source, XPathConstants.BOOLEAN);
+				Boolean exists = (Boolean)this.xpath.evaluate(xpathExpression, source, XPathConstants.BOOLEAN);
 				return exists;
 			}
-
 		} catch (XPathExpressionException ex) {
 			// Log the exception and continue.
 			log.error("Error evaluating xpath expression: '"+xpathExpression+"'", ex);
@@ -124,16 +73,14 @@ public class XPathUtils {
 		return false;
 	}
 
-	public static String getString(Object source, String xpathExpression) {
+	public String getString(Object source, String xpathExpression) {
 		try {
 			if (source != null) {
-				XPath xpath = getXPathInstance();
-				Node node = (Node) xpath.evaluate(xpathExpression, source, XPathConstants.NODE);
+				Node node = (Node)this.xpath.evaluate(xpathExpression, source, XPathConstants.NODE);
 				if (node != null) {
 					return node.getTextContent();
 				}
 			}
-
 		} catch (XPathExpressionException ex) {
 			// Log the exception and continue.
 			log.error("Error evaluating xpath expression: '"+xpathExpression+"'", ex);
@@ -144,14 +91,12 @@ public class XPathUtils {
 		return null;
 	}
 
-	public static Node getNode(Object source, String xpathExpression) {
+	public Node getNode(Object source, String xpathExpression) {
 		try {
 			if (source != null) {
-				XPath xpath = getXPathInstance();
-				Node node = (Node) xpath.evaluate(xpathExpression, source, XPathConstants.NODE);
+				Node node = (Node)this.xpath.evaluate(xpathExpression, source, XPathConstants.NODE);
 				return node;
 			}
-
 		} catch (XPathExpressionException ex) {
 			// Log the exception and continue.
 			log.error("Error evaluating xpath expression: '"+xpathExpression+"'", ex);
@@ -162,14 +107,12 @@ public class XPathUtils {
 		return null;
 	}
 
-	public static NodeList getNodeList(Object source, String xpathExpression) {
+	public NodeList getNodeList(Object source, String xpathExpression) {
 		try {
 			if (source != null) {
-				XPath xpath = getXPathInstance();
-				NodeList nodeList = (NodeList) xpath.evaluate(xpathExpression, source, XPathConstants.NODESET);
+				NodeList nodeList = (NodeList)this.xpath.evaluate(xpathExpression, source, XPathConstants.NODESET);
 				return nodeList;
 			}
-
 		} catch (XPathExpressionException ex) {
 			// Log the exception and continue.
 			log.error("Error evaluating xpath expression: '"+xpathExpression+"'", ex);
@@ -181,14 +124,12 @@ public class XPathUtils {
 	}
 
 	// Get node list with xPath instance for 'IDF'
-	public static NodeList getNodeList(Object source, String xpathExpression, NamespaceContext nsContext) {
+	public NodeList getNodeList(Object source, String xpathExpression, NamespaceContext nsContext) {
 		try {
 			if (source != null) {
-				XPath xpath = getXPathInstance(nsContext);
-				NodeList nodeList = (NodeList) xpath.evaluate(xpathExpression, source, XPathConstants.NODESET);
+				NodeList nodeList = (NodeList)this.xpath.evaluate(xpathExpression, source, XPathConstants.NODESET);
 				return nodeList;
 			}
-
 		} catch (XPathExpressionException ex) {
 			// Log the exception and continue.
 			log.error("Error evaluating xpath expression: '"+xpathExpression+"'", ex);
@@ -209,7 +150,7 @@ public class XPathUtils {
 	 * @param xpath
 	 * @return The last added node
 	 */
-	public static Node createElementFromXPath(Node node, String xpath) {
+	public Node createElementFromXPath(Node node, String xpath) {
 		Node refNode = null;
 		if (xpath.startsWith("/")) {
 			refNode = node.getOwnerDocument().getDocumentElement();
@@ -222,15 +163,15 @@ public class XPathUtils {
 		Node result = refNode;
 		for (int i=0; i<xpathElements.length; i++) {
 			if (xpathElements[i].length() > 0) {
-				if (!XPathUtils.nodeExists(refNode, tmpXpath + "/" + xpathElements[i])) {
+				if (!this.nodeExists(refNode, tmpXpath + "/" + xpathElements[i])) {
 					if (tmpXpath.length() == 0) {
 						throw new IllegalArgumentException("More than one root element is not allowed! The supplied absolute path MUST start with the existing root node!");
 					} else {
-						NodeList list = XPathUtils.getNodeList(refNode, tmpXpath);
+						NodeList list = this.getNodeList(refNode, tmpXpath);
 						result = list.item(0).appendChild(refNode.getOwnerDocument().createElement(xpathElements[i]));
 					}
 				} else if (tmpXpath.length() > 0) {
-					result = XPathUtils.getNodeList(refNode, tmpXpath+ "/" + xpathElements[i]).item(0);
+					result = this.getNodeList(refNode, tmpXpath+ "/" + xpathElements[i]).item(0);
 				}
 				tmpXpath = tmpXpath + "/" + xpathElements[i];
 			} else {
@@ -240,9 +181,9 @@ public class XPathUtils {
 		return result;
 	}
 
-	public static Node createElementFromXPathAsSibling(Node node, String xpath) {
-		boolean createSibling = XPathUtils.nodeExists(node, xpath);
-		Node n = createElementFromXPath(node, xpath);
+	public Node createElementFromXPathAsSibling(Node node, String xpath) {
+		boolean createSibling = this.nodeExists(node, xpath);
+		Node n = this.createElementFromXPath(node, xpath);
 		if (createSibling) {
 			n = XMLUtils.insertAfter(node.getOwnerDocument().createElement(xpath.split("/")[xpath.split("/").length-1]), n);
 		}
