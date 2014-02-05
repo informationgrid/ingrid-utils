74cc1a9adb6ba1de853232fc0ea14790d9ec2621
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-02-05 14:49:35 +0000

    - added Namespace for WFS version 1.1.0 - renamed Namespace to one with version included

--- src/main/java/de/ingrid/utils/xml/Wfs110NamespaceContext.java
+++ src/main/java/de/ingrid/utils/xml/Wfs110NamespaceContext.java
@@ -12,10 +12,12 @@ import javax.xml.namespace.NamespaceContext;
  * @author joachim
  *
  */
-public class WfsNamespaceContext implements NamespaceContext {
+public class Wfs110NamespaceContext implements NamespaceContext {
 
-	public static String NAMESPACE_URI_WFS = "http://www.opengis.net/wfs/2.0";
-	public static String NAMESPACE_URI_OWS11 = "http://www.opengis.net/ows/1.1";
+	public static String NAMESPACE_URI_WFS = "http://www.opengis.net/wfs";
+	public static String NAMESPACE_URI_OWS = "http://www.opengis.net/ows";
+	public static String NAMESPACE_URI_GML = "http://www.opengis.net/gml";
+	public static String NAMESPACE_URI_OGC = "http://www.opengis.net/ogc";
 	public static String NAMESPACE_URI_XLINK = "http://www.w3.org/1999/xlink";
 	public static String NAMESPACE_URI_INSPIRE_COMMON = "http://inspire.ec.europa.eu/schemas/common/1.0";
 	public static String NAMESPACE_URI_INSPIRE_DLS = "http://inspire.ec.europa.eu/schemas/inspire_dls/1.0";
@@ -29,8 +31,12 @@ public class WfsNamespaceContext implements NamespaceContext {
 	public String getNamespaceURI(String prefix) {
 		if (prefix.equals("wfs")) {
             return NAMESPACE_URI_WFS;
-		} else if (prefix.equals("ows11")) {
-		    return NAMESPACE_URI_OWS11;
+		} else if (prefix.equals("ows")) {
+		    return NAMESPACE_URI_OWS;
+		} else if (prefix.equals("gml")) {
+		    return NAMESPACE_URI_GML;
+		} else if (prefix.equals("ogc")) {
+		    return NAMESPACE_URI_OGC;
 		} else if (prefix.equals("inspire_dls")) {
             return NAMESPACE_URI_INSPIRE_DLS;
         } else if (prefix.equals("inspire_common")) {
