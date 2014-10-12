8510f68b49f40a60014a3d06455cf1ea474f65a6
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-10-12 18:42:26 +0000

    - add plugdescription filter capabilities to be able to filter plugdescription elements per iPlug type - remove partner/provider selection for iPlug SE, all partner/provider are used - add grouping support for new iPLug SE

---
+++ src/main/java/de/ingrid/utils/IPlugDescriptionFilter.java
@@ -0,0 +1,11 @@
+package de.ingrid.utils;
+
+public interface IPlugDescriptionFilter {
+
+    /**
+     * Filters the plugdescription
+     * 
+     * @param plugDescription
+     */
+    void filter(PlugDescription plugDescription);
+}
