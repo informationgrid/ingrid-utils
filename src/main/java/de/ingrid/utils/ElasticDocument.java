97c9e4504addf38d3be80ad53d1731be14ca1aa4
Andre Wallat <andre.wallat@wemove.com>
2015-04-17 15:09:08 +0200

    - introduction of class ElasticDocument as exchange format

---
+++ src/main/java/de/ingrid/utils/ElasticDocument.java
@@ -0,0 +1,74 @@
+package de.ingrid.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ElasticDocument extends HashMap<String, Object>{
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -9176076545333543500L;
+    
+    public ElasticDocument() {}
+    
+    public ElasticDocument( Map<String, Object> map ) {
+        super( map );
+    }
+
+    /**
+     * Add a key with its value to the analyzed document, which will be used for indexing.
+     * If the key is already present in the document, then the old value will be converted
+     * to a list and the new value appended to it.
+     * 
+     * @param key is the name of the field to be stored
+     * @param value is the value of the field
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public Object put(String key, Object value) {
+        Object obj = get( key );
+        // if key does not exist yet then just add key/value to document
+        if (obj == null) {
+            super.put( key, value );
+        } else {
+            // if the object behind the key already is an array, we just add the value to this array
+            if (obj instanceof List<?>) {
+                ((List<Object>) obj).add( value );
+            } else {
+                // this is the second time where the same key is added, so we have to convert the object
+                // into a list of objects
+                ArrayList<Object> list = new ArrayList<Object>();
+                list.add( obj );
+                list.add( value );
+                super.put( key, list );
+            }
+        }
+        return get( key );
+    }
+    
+    /**
+     * Get the value(s) for a given key as a string array. If the value is null, an empty array
+     * will be returned. If the value is a string, an array with one entry is returned. If the
+     * value is a list, then it will be converted to an array.
+     * @param key is the key to look for value(s)
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public String[] getValues(String key) {
+        Object obj = get( key );
+        String[] result = null;
+        
+        if (obj == null) {
+            result = new String[0];
+        } else if (obj instanceof List<?>) {
+            return ((List<Object>) obj).toArray( new String[0] );
+        } else {
+            result = new String[] { (String) obj };
+        }
+        return result;
+    }
+
+}
