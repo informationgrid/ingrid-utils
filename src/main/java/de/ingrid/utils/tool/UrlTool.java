7b2aafb47d41d97dd4866ebc2b3cc7278e405c45
Joachim Müller <joachim.mueller@wemove.com>
2019-07-04 01:52:36 +0200

    Add URL IDN and UniCode support.
    New class that encodes and decodes URLs with Unicode characters
    in domain or path parts.

---
+++ src/main/java/de/ingrid/utils/tool/UrlTool.java
@@ -0,0 +1,116 @@
+/*
+ * **************************************************-
+ * ingrid-utils
+ * ==================================================
+ * Copyright (C) 2014 - 2019 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * **************************************************#
+ */
+/**
+ * 
+ */
+package de.ingrid.utils.tool;
+
+import com.ibm.icu.text.IDNA;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+/**
+ * @author joachim@wemove.com
+ * 
+ */
+public class UrlTool {
+
+    private static final Log LOG = LogFactory.getLog(UrlTool.class);
+
+    /**
+     * @return Returns the URL with
+     *    <ul>
+     *      <li>the urls domain is IDN encoded (using IDNA 2008, nontransitional processing)</li>
+     *      <li>the path, query and anchor is US-ASCII encoded</li>
+     *    </ul>
+     *
+     *    If the url was already encoded, return the URL as is.
+     *
+     * @throws MalformedURLException
+     * @throws URISyntaxException
+     */
+    public static String getIdnUrlWithEncodedPath(String urlStr) throws MalformedURLException, URISyntaxException {
+
+        // return if ascii only url
+        // this also returns for already encoded urls
+        if (Charset.forName("US-ASCII").newEncoder().canEncode(urlStr)) {
+            return urlStr;
+        }
+
+        URL u = new URL(urlStr);
+
+        final IDNA.Info idnaInfo = new IDNA.Info();
+        final StringBuilder idnaOutput = new StringBuilder();
+        IDNA.getUTS46Instance(IDNA.NONTRANSITIONAL_TO_ASCII).nameToASCII(u.getHost(), idnaOutput, idnaInfo);
+
+        URI uri = new URI(
+                u.getProtocol(),
+                null,
+                idnaOutput.toString(),
+                u.getPort(),
+                u.getPath(),
+                u.getQuery(),
+                u.getRef());
+
+        return uri.toASCIIString();
+    }
+
+
+    /**
+     * @return Returns the URL with
+     *    <ul>
+     *      <li>the urls domain is IDN decoded (using IDNA 2008, nontransitional processing)</li>
+     *      <li>the path, query and anchor is URI decoded</li>
+     *    </ul>
+     *
+     * @throws MalformedURLException
+     * @throws URISyntaxException
+     */
+    public static String getDecodedIdnUrl(String urlStr) throws MalformedURLException, URISyntaxException {
+
+        URI uri = new URI(urlStr);
+
+        final IDNA.Info idnaInfo = new IDNA.Info();
+        final StringBuilder idnaOutput = new StringBuilder();
+        IDNA.getUTS46Instance(IDNA.NONTRANSITIONAL_TO_UNICODE).nameToUnicode(uri.getHost(), idnaOutput, idnaInfo);
+
+        String s = uri.getScheme() + "://"
+                + (uri.getUserInfo() != null ? uri.getUserInfo() : "")
+                + idnaOutput.toString()
+                + (uri.getPort() != -1 && uri.getPort() != 80 ? ":" + uri.getPort() : "")
+                + uri.getPath()
+                + (uri.getQuery() != null ? "?" + uri.getQuery() : "")
+                + (uri.getFragment() != null ? "#" + uri.getFragment() : "");
+
+
+        return s;
+    }
+
+
+}
---
+++ src/test/java/de/ingrid/utils/tool/UrlToolTest.java
@@ -0,0 +1,62 @@
+/*
+ * **************************************************-
+ * ingrid-utils
+ * ==================================================
+ * Copyright (C) 2014 - 2019 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * **************************************************#
+ */
+package de.ingrid.utils.tool;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+public class UrlToolTest {
+
+    @Test
+    public void getIdnUrlWithEncodedPath() throws MalformedURLException, URISyntaxException {
+
+        Assert.assertEquals("http://www.domain.de/F%C3%B6n/Frisur?mu%C3%9F=ja&preis=gro%C3%9F#Au%C3%9Fz%C3%BCge", UrlTool.getIdnUrlWithEncodedPath("http://www.domain.de/Fön/Frisur?muß=ja&preis=groß#Außzüge"));
+
+        Assert.assertEquals("http://www.xn--schnheit-p4a.de/", UrlTool.getIdnUrlWithEncodedPath("http://www.schönheit.de/"));
+
+        Assert.assertEquals("http://www.xn--sigkeiten-g1a23a.de/F%C3%B6n/Frisur?mu%C3%9F=ja&preis=gro%C3%9F#Au%C3%9Fz%C3%BCge", UrlTool.getIdnUrlWithEncodedPath("http://www.süßigkeiten.de/Fön/Frisur?muß=ja&preis=groß#Außzüge"));
+
+        Assert.assertEquals( "http://www.geilenkirchen.de/stadtplanung/bauleitplanung/rechtskraeftige-bauleitpl%C3%A4ne/", UrlTool.getIdnUrlWithEncodedPath("http://www.geilenkirchen.de/stadtplanung/bauleitplanung/rechtskraeftige-bauleitpl%C3%A4ne/"));
+
+        Assert.assertEquals( "http://www.geilenkirchen.de/stadtplanung/bauleitplanung/rechtskraeftige-bauleitpl%C3%A4ne/", UrlTool.getIdnUrlWithEncodedPath("http://www.geilenkirchen.de/stadtplanung/bauleitplanung/rechtskraeftige-bauleitpläne/"));
+
+        Assert.assertEquals("http://www.xn--schnheit-p4a.de:8800/", UrlTool.getIdnUrlWithEncodedPath("http://www.schönheit.de:8800/"));
+
+        Assert.assertEquals("https://www.xn--schnheit-p4a.de/", UrlTool.getIdnUrlWithEncodedPath("https://www.schönheit.de/"));
+
+    }
+
+    @Test
+    public void getDecodedIdnUrl() throws MalformedURLException, URISyntaxException {
+
+        Assert.assertEquals("http://www.süßigkeiten.de/Fön/Frisur?muß=ja&preis=groß#Außzüge", UrlTool.getDecodedIdnUrl("http://www.xn--sigkeiten-g1a23a.de/F%C3%B6n/Frisur?mu%C3%9F=ja&preis=gro%C3%9F#Au%C3%9Fz%C3%BCge"));
+
+        Assert.assertEquals("http://www.schönheit.de:8800/", UrlTool.getDecodedIdnUrl("http://www.xn--schnheit-p4a.de:8800/"));
+
+        Assert.assertEquals("https://www.schönheit.de/", UrlTool.getDecodedIdnUrl("https://www.xn--schnheit-p4a.de/"));
+    }
+
+}
